de5047f7d20fc1957998b8f0ae9c1b60045d039b
krumaska <51515055+krumaska@users.noreply.github.com>
2019-11-15 19:15:29 +0900

    8-1 Complete

--- Week_8/Homework 8-1/Main.java
+++ Week_8/Homework 8-1/Main.java
@@ -3,15 +3,20 @@ class Pen { // 모든 펜의 공통 속성
     public int getAmount() { return amount; }
     public void setAmount (int amount) { this.amount = amount; }
  }
- class SharpPencil extends Pen{ // 샤프펜슬
-    // fill here!  
+ class SharpPencil extends Pen { // 샤프펜슬
+    // fill here!
+    private int width;
  } 
  
  class Ballpen extends Pen { // 볼펜
     // fill here!  
+    private String color;
+    public String getColor() { return color; }
+    public void setColor(String color) { this.color = color; }
  }
  class FountainPen extends Ballpen{ // 만년필
     // fill here!  
+    public void refill (int n) { setAmount(n); }
  }
  
  class Main {
