0bb82b10471fdda87393438857f7b8133e10afab
krumaska <51515055+krumaska@users.noreply.github.com>
2019-12-01 10:42:03 +0900

    Delete and Update 9-1

--- Week_9/Homework 9-1/Main.java
+++ Week_9/Homework 9-1/Main.java
@@ -5,11 +5,11 @@ class Main {
   
 	public static String len_check(String str){
 	  // fill here! 
-	  if(str.length() > 20) {
-		  throw new Exception("More than 20 words");
-	  } else return str;
+	  	if(str == null) throw new RuntimeException("null");
+		else if(str.length()>20) throw new RuntimeException("More than 20 words");
+		else return str; 
 	}
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args){
 
 		System.out.println(len_check("abcdefghijklmnopqrs"));//abcdefghijklmnopqrs
 		System.out.println(len_check("abcdefghijklmnopqrsuvw"));
